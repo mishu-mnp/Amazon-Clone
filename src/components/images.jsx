2d22216b6d38e979c95469e8f3202c64081466b1
Vaibhav Mishra <mishravaibhav9212@gmail.com>
2021-11-18 01:38:45 +0530

    logo changes

--- src/components/images.jsx
+++ src/components/images.jsx
@@ -8,6 +8,10 @@ import css from '../img/css.jpg';
 import javascript from '../img/javascript.jfif';
 import reactjs from '../img/reactjs.jpg';
 import checkout from '../img/checkout.jpg';
+import amazonlogo from '../img/amazonlogo.png'
+import amazonlogo2 from '../img/amazonlogo2.png'
+import amazonlogo3 from '../img/amazonlogo3.png'
+import amazonlogo4 from '../img/amazonlogo4.png'
 
 const images = {
     logo: logo,
@@ -20,5 +24,9 @@ const images = {
     javascript: javascript,
     reactjs: reactjs,
     checkout: checkout,
+    amazonlogo: amazonlogo,
+    amazonlogo2: amazonlogo2,
+    amazonlogo3: amazonlogo3,
+    amazonlogo4: amazonlogo4,
 }
 export default images
